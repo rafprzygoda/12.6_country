649df80346be1a903ac68515634ec9e6869f5364
rafal <ra88fi@o2.pl>
2017-06-25 12:27:51 +0200

    countries

---
+++ script.js
@@ -0,0 +1,29 @@
+$(function() {
+    var url = 'https://restcountries.eu/rest/v1/name/';
+    var capital = 'https://restcountries.eu/rest/v1/capital/'
+    var countriesList = $('#countries');
+    
+    
+    function searchCountries() {
+        var countryName = $('#country-name').val();
+        
+        if(!countryName.length) {
+            countryName = 'Poland';
+        }
+        
+        $.ajax({
+            url: url + countryName,
+            method: 'GET',
+            success: showCountriesList
+        });
+    }
+    
+    $('#search').click(searchCountries);
+    
+     function showCountriesList(resp) {
+        countriesList.empty();
+        resp.forEach(function(item) {
+            $('<li>').text(item.name).appendTo(countriesList);
+        });
+    }
+});
